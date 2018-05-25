17ec3a863011d9d0fc888dfe760c2e904217b3ac
Jess Jacobson <jessanova@gmail.com>
2018-05-25 18:04:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function addToCart(item) {
 
 function viewCart() {
   if(cart.length===0){return "Your shopping cart is empty."}
-  var s = `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}` 
+  var s = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}` 
   if(cart.length>1){
   for(var i=1;i<cart.length-1;i++){
     s+=`, ${cart[i].itemName} at $${cart[i].itemPrice}` 
