bd360ead0816050afdec3f1af0ff50789c070836
Jess Jacobson <jessanova@gmail.com>
2018-05-25 19:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i=0;i<cart.length;i++){
+    if(item==cart[i].itemName){cart.splice(i,1);return cart
+  }
 }
 
 function placeOrder(cardNumber) {
