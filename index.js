0dd0c88ae2ac37d8e1c1a8a091723e515931a7c8
Jess Jacobson <jessanova@gmail.com>
2018-05-25 16:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ function setCart(c) {
 function addToCart(item) {
   console.log(item)
   var itemName=`${item}`
-  //console.log(x)
   var itemPrice=`${Math.floor(Math.random()*100)}`
-  //console.log(y)
   cart.push({itemName,itemPrice})
   console.log(cart)
   return `${item} has been added to your cart.`
